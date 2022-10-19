b3631e541d77ba533b6d88f021b6279e129ded79
Ward Muylaert <ward.muylaert@gmail.com>
2022-10-19 14:28:00 +0200

    Maps: ability to use unicode symbols on map
    Also a bit of a cleanup of that code. Still a mess though.

--- maps/script.js
+++ maps/script.js
@@ -1,9 +1,10 @@
-const DEFAULT_MAP = 'visited';
+const DEFAULT_MAP = "visited";
+var mymap;
 
 function loadJSON(callback, geojson_location) {
   var xobj = new XMLHttpRequest();
   xobj.overrideMimeType("application/json");
-  xobj.open('GET', geojson_location, true);
+  xobj.open("GET", geojson_location, true);
   xobj.onreadystatechange = function () {
     if (xobj.readyState == 4 && xobj.status == "200") {
       callback(JSON.parse(xobj.responseText));
@@ -20,26 +21,48 @@ function createPopup(feature, layer) {
     layer.bindPopup(popupContent);
   }
 }
+// Enable placing custom points. By default just use the L.marker, which is
+// what Leaflet would do normally.
+function create_point(geo_json_point, latlng) {
+  if (geo_json_point.properties && geo_json_point.properties.unicode_icon) {
+    let unicode_icon = L.divIcon({
+      html: geo_json_point.properties.unicode_icon,
+      iconSize: [30, 30],
+      className: "unicode_icon",
+    });
+    return L.marker(latlng, { icon: unicode_icon });
+  } else {
+    return L.marker(latlng);
+  }
+}
+// Uses global mymap variable
 function handle_geojson(geojson) {
-  var mymap = L.map('mapid').setView([39,-38], 3);
-  L.tileLayer('https://a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-    maxZoom: 18,
+  // The style option is for lines and polygons, not for points. Instead, use pointToLayer.
+  var geojsonLayer = L.geoJSON(geojson, {
+    pointToLayer: create_point,
+    onEachFeature: createPopup,
   }).addTo(mymap);
-
-  var geojsonLayer = L.geoJSON(geojson, { onEachFeature: createPopup }).addTo(mymap);
   L.control.scale({ metric: true, imperial: false }).addTo(mymap);
   mymap.fitBounds(geojsonLayer.getBounds());
+}
+// Overrides global mymap variable
+function create_map() {
+  mymap = L.map("mapid").setView([39, -38], 3);
+  L.tileLayer("https://a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+    // maxZoom: 18,
+  }).addTo(mymap);
 }
 
 // () -> String
 function get_map_name() {
   var params = new URLSearchParams(window.location.search);
-  var map_name = params.get('i') || DEFAULT_MAP;
+  var map_name = params.get("i") || DEFAULT_MAP;
   return `geojson/${map_name}.geojson`;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
+  create_map();
   loadJSON(handle_geojson, get_map_name());
 });
-
