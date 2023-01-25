3b4c90d766301a239e4c261a1dc7b78ae431f167
Ward Muylaert <ward.muylaert@gmail.com>
2023-01-25 13:45:21 +0100

    Add quick page showing countries I've been on map

---
+++ maps/countries/script.js
@@ -0,0 +1,71 @@
+var mymap;
+const VISITED_COUNTRIES = [
+  "belgium",
+  "netherlands",
+  "luxembourg",
+  "france",
+  "germany",
+  "united_kingdom",
+  "spain",
+  "monaco",
+  "italy",
+  "slovenia",
+  "denmark",
+  "sweden",
+  "norway",
+  "greece",
+  "malta",
+  "cyprus",
+  "usa",
+  "switzerland",
+  "austria",
+  "ireland",
+  "argentina",
+  "czech", // No GPS tracks alas
+];
+// Country data from https://github.com/georgique/world-geojson. Only
+// downloaded those countries I have been in at the time of writing.
+
+function loadJSON(callback, geojson_location) {
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open("GET", geojson_location, true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      callback(JSON.parse(xobj.responseText));
+    }
+  };
+  xobj.send(null);
+}
+function handle_country(country_name, geojson) {
+  var geojsonLayer = L.geoJSON(geojson, {
+    // pointToLayer: create_point,
+    onEachFeature: (feature, layer) =>
+      create_popup(feature, layer, country_name),
+  }).addTo(mymap);
+}
+function create_popup(feature, layer, country_name) {
+  if (typeof country_name === "string" && country_name !== "") {
+    layer.bindPopup(country_name);
+  }
+}
+function create_map() {
+  mymap = L.map("mapid").setView([39, -38], 3);
+  L.tileLayer("https://a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+    // maxZoom: 18,
+  }).addTo(mymap);
+  L.control.scale({ metric: true, imperial: false }).addTo(mymap);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  create_map();
+
+  for (let country of VISITED_COUNTRIES) {
+    loadJSON(
+      (geojson) => handle_country(country, geojson),
+      `geojson/${country}.geojson`
+    );
+  }
+});
